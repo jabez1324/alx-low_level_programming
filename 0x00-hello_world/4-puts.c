b7db1f7db6f430345ee2a85b70ab79fab7ae7287
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-14 05:09:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * This function is the entry point of the program. It calls the `puts` function
- *     * to print a message to the standard output.
- *      *
- *       * Return: Always 0 (Success)
- *        */
+ * main - Entry point
+ *
+ * This function is the entry point of the program. It calls the `puts` function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/* ... */
