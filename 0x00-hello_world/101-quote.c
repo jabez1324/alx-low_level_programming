fc7c6d27873f2a2e891e703725192134f717fb0f
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-14 06:39:34 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function is the entry point of the program. It initializes any
+ * necessary data structures, processes command line arguments, and
+ * starts the main program loop.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main (void)
+{
+	fprintf(stderr"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
