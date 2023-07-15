690d9b7be4765c3de4c2941b493f3a8adc2804d9
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-14 23:14:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * This function is the entry point program. It calls the puts function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str = 'a';
+	int num = 26;
+
+	for (i = 0; i < num; i++)
+	{
+		if (str + i == 'e' || str + i == 'q')
+		{
+			continue;
+		}
+		putchar(str + i);
+	}
+	putchar("\n");
+
+	return (0);
+}
