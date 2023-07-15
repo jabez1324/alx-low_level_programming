f43f961c2fb6e8af727ad5a8bbfc26057d334750
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-15 00:18:21 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function is the entry point program. It calls the puts function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str = 'z';
+	int num = 26;
+
+	for (i = 0; i < num; i++)
+	{
+		putchar(str - i)
+	}
+	putchar('\n');
+
+	return (0):
+}
