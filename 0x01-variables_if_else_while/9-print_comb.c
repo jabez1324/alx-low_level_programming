0bf783054cedddfc3e769db4b208e48f6591f2eb
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-15 01:17:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function is the entry point program. It calls the puts function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(' ');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
