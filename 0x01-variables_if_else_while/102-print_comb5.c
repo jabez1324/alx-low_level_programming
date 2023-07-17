540d79b441af801349d6d802acf8fa63ef9fee18
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-17 00:40:16 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			int i1 = i / 10;
+			int i2 = i % 10;
+			int j1 = j / 10;
+			int j2 = j % 10;
+			putchar('0' + i1);
+			putchar('0' + i2);
+			putchar(' ');
+			putchar('0' + j1);
+			putchar('0' + j2);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}	
+	putchar('\n');	
+
+	return (0);
+}
