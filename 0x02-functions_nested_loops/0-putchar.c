3af75504ed8e20ded3eba178866bf5df4dabc352
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-18 00:31:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function is the entry point program. It calls the puts function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+THis is the readme of the function directory
