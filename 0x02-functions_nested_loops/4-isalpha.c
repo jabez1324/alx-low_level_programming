e4ace7f35524baa6c58ada4ddd495c8b93c5b463
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 01:35:46 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <ctype.h>
+#inlcude "main.h"
+
+/**
+ * _islower - Prints the lowercase letters of the alphabet
+ * @c: the character to check
+ * Description: This function prints the lowercase letters of the alphabet
+ * to the console, separated by spaces.
+ *
+ * Return: This function does not return a value.
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
