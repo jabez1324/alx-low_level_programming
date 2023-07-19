f1c82abb136fc7afce694607d4fe8b7b05f754fa
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 05:01:33 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,16 +17,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		write(1, "+", 1);
-		return (1);
+		return 1;
 	}                             
 	else if (n == 0)                
 	{
 		write(1, "0", 1);
-		return (0);
+		return 0;
 	}
 	else if (n < 0)                        
 	{
 		write(1, "-", 1);
-		return (-1);
+		return -1;
 	}
 }
