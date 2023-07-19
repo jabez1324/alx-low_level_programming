f5917e5b8157c8aa024ecb6312515c0f4af0a78e
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 02:00:01 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**print_sign - Entry point
+ *
+ * @n: the character to check
+ *
+ * This function is the entry point program. It calls the puts function
+ * to print a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{                              
+		return (1);                                                                       
+	        _putchar(+);                                          
+	}                                                                         
+	else if (n = 0)                
+	{                                                                             
+		return (0);                                                                     
+	     	_putchar(0);                                                             
+	}
+	else if(n < 0)                        
+	{  
+		return (-1);
+		-putchar(-);
+	}
