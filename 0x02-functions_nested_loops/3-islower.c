44e703ed3d776a55019598efd9f11f6ce2d95511
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 00:48:15 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <ctype.h>
-
-/**
- * main - Entry point
- *
- * This function is the entry point program. It calls the puts function
- * to print a message to the standard output.
- *
- * Return: Always 0 (Success)
- * .
- */
-int _islower(int c);
-
-int main(void)
-{
-	_islower(int c);
-	return (0);
-}
+#include "main.h"
 
 /**
  * print_alphabet - Prints the lowercase letters of the alphabet
@@ -26,14 +10,15 @@ int main(void)
  *
  * Return: This function does not return a value.
  */
+
 int _islower(int c)
 {
 	if (islower(c))
 	{
-		putchar('1');
+		_putchar('1');
 	}	
 	else
 	{
-		putchar('0');
+		_putchar('0');
 	}
 }
