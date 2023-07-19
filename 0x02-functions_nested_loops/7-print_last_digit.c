db59022945904dc2006f4e69a58aeafd883cc744
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 05:45:56 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+
+/**
+ * _isalpha - Prints the lowercase letters of the alphabet
+ * @c: the character to check
+ * Description: This function prints the lowercase letters of the alphabet
+ * to the console, separated by spaces.
+ *
+ * Return: This function does not return a value.
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
