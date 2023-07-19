0932942081883fa4abc6839cdf5642dcd2b90e83
Jabez Shiferaw <jabezshiferaw31@gmail.com>
2023-07-19 05:35:50 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * _abs - Prints the lowercase letters of the alphabet
+ * @i: the character to check
+ * Description: This function prints the lowercase letters of the alphabet
+ * to the console, separated by spaces.
+ *
+ * Return: This function does not return a value.
+ */
+
+
 int _abs(int i)
 {
 	return abs(i);
